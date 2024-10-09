55bf441058c5d37202ecc664607edc4ebdd819b7
HoangDen2003 <hadinhhoang031104@gmail.com>
2024-10-10 01:39:48 +0700

    refactor config

--- config/db_connection.php
+++ config/db_connection.php
@@ -3,11 +3,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "maga_translated";
+$dbname = "manga_translated";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
-?>
